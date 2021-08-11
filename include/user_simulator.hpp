02ab2cc53e7b3e1e41d78a2ad3dfef07bcc8787e
pingping <77882276+pingpinger-ing@users.noreply.github.com>
2021-08-11 22:38:44 +0100

    Update user_simulator.hpp

--- include/user_simulator.hpp
+++ include/user_simulator.hpp
@@ -155,6 +155,8 @@ private:
         return act;
         */
          bool act = false;
+         std::vector< std::vector<edge*> > batches_all;
+        batches_all = create_batches();
          
           for(unsigned i = 0; i != batches_all.size(); ++i){
          tbb::parallel_for(tbb::blocked_range<unsigned>(0,(unsigned)batches_all[i].size(), 512), [&](const tbb::blocked_range<unsigned>& range) { 
@@ -369,8 +371,6 @@ private:
     
     bool step_all()
     {       
-        std::vector< std::vector<edge*> > batches_all;
-        batches_all = create_batches();
 
         log(2, "stepping edges");
         bool active=false;
