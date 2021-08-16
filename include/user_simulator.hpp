e6dfa567ec9a48d12c98bbd6a4ecc6bf51617df4
pingping <77882276+pingpinger-ing@users.noreply.github.com>
2021-08-16 23:03:34 +0100

    Update user_simulator.hpp

--- include/user_simulator.hpp
+++ include/user_simulator.hpp
@@ -384,10 +384,10 @@ private:
         log(2, "stepping edges");
         bool active=false;
         
-        std::cout<<batches_all[0].size()<<endl;
-        std::cout<<batches_all[1].size()<<endl;
-        std::cout<<batches_all[2].size()<<endl;
-        std::cout<<batches_all[3].size()<<endl;
+        std::cout<<batches_all[0].size()<<std::endl;
+        std::cout<<batches_all[1].size()<<std::endl;
+        std::cout<<batches_all[2].size()<<std::endl;
+        std::cout<<batches_all[3].size()<<std::endl;
         
         
         for(unsigned i = 0; i != batches_all.size(); ++i){
