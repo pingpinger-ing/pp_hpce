aa7d0df1d92ca5ad93d4687d36f3c54a37f26cae
pingping <77882276+pingpinger-ing@users.noreply.github.com>
2021-08-23 11:43:51 +0100

    Update user_simulator.hpp

--- include/user_simulator.hpp
+++ include/user_simulator.hpp
@@ -387,13 +387,13 @@ private:
               else {
                   if (srcIndex == dstIndex + 1) {
                       batches_all[2].push_back(&m_edges[i_edge]);
-                      std::cout << "分区3"<< std::endl; 
-                      std::cout << srcIndex << dstIndex << std::endl;
+                      //std::cout << "分区3"<< std::endl; 
+                      //std::cout << srcIndex << dstIndex << std::endl;
                   }
                   else {
                       batches_all[3].push_back(&m_edges[i_edge]);
-                     // std::cout << "分区4"<< std::endl; 
-                      //std::cout << srcIndex << dstIndex << std::endl;
+                      std::cout << "分区4"<< std::endl; 
+                      std::cout << srcIndex << dstIndex << std::endl;
                   }
               }        
               
