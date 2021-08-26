0407047446ffe1a8b755e20b0095a0a288e81d95
pingping <77882276+pingpinger-ing@users.noreply.github.com>
2021-08-26 15:06:23 +0100

    Update user_simulator.hpp

--- include/user_simulator.hpp
+++ include/user_simulator.hpp
@@ -472,7 +472,7 @@ void DFS(int v)
     for (int i = 0; i != adj[v].size(); i++){
         if (!visited[adj[v][i]]){
           for (unsigned j = 0; j!=m_edges.size(); j++){
-            if(m_edges[j].srcindex == v && m_edges[j].dstindex == visited[adj[v][i]){
+            if(m_edges[j].srcindex == v && m_edges[j].dstindex == visited[adj[v][i]]){
                batches_all[count].push_back(&m_edges[j]); 
                adj[v].erase(i);
                std::cout<<batches_all[count].size()<<std::endl; 
